80570053df81ad517de4aca25f414f7a8e768145
Mike Huang <wellwind@mail2000.com.tw>
2023-06-10 12:07:32 +0800

    任務 03：在程式中取得路由參數

--- src/app/pages/posts/post/post.component.ts
+++ src/app/pages/posts/post/post.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from 'src/app/post.service';
+import { Article } from 'src/app/interfaces/article';
+import { map, shareReplay, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +12,32 @@ import { CommonModule } from '@angular/common';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export default class PostComponent {
+export default class PostComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  postService = inject(PostService);
 
+  article?: Article;
+  article$ = this.route.paramMap.pipe(
+    // 取得路由參數
+    map((params) => params.get('id') || ''),
+    // 呼叫 API
+    switchMap((id) => this.postService.getArticle(id)),
+    // 將結果的 article 儲存起來
+    map(result => result.article),
+    // 避免畫面重複訂閱
+    shareReplay(1)
+  );
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      // 取得路由參數
+      const id = params.get('id') || '';
+      console.log(id);
+      // 呼叫 API
+      this.postService.getArticle(id).subscribe((data) => {
+        // 將結果的 article 儲存起來
+        this.article = data.article;
+      });
+    });
+  }
 }
--- src/app/pages/posts/posts/posts.component.ts
+++ src/app/pages/posts/posts/posts.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { PostService } from './../../../post.service';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Article } from 'src/app/interfaces/article';
+import { RouterLink } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export default class PostsComponent {
+export default class PostsComponent implements OnInit {
 
+  postService = inject(PostService);
+
+  articles: Article[] = [];
+
+  articles$ = this.postService.getArticles().pipe(
+    map((data) => data.articles)
+  );
+
+  ngOnInit(): void {
+    this.postService.getArticles().subscribe((data) => {
+      this.articles = data.articles;
+    });
+  }
 }
