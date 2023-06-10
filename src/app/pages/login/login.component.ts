f5c1496b0c673113df852db09d46aebfd158aeeb
Mike Huang <wellwind@mail2000.com.tw>
2023-06-10 13:44:14 +0800

    任務 04：在程式中動態切換路由

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from 'src/app/login.service';
+import { map } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export default class LoginComponent {
+  router = inject(Router);
+  loginService = inject(LoginService);
 
+  user = {
+    email: '',
+    password: '',
+  };
+
+  login() {
+    console.log(this.user);
+    this.loginService
+      .login(this.user)
+      .pipe(map((data) => data.user.token))
+      .subscribe({
+        next: (token) => {
+          console.log(token);
+          localStorage.setItem('token', token);
+          this.router.navigateByUrl('/');
+        },
+        error: (err: HttpErrorResponse) => {
+          console.log(err.error.body[0]);
+          alert(err.error.body[0]);
+        }
+      });
+  }
 }
