4ad2d77adf4048c03160a393fe209f773aaffd66
Mike Huang <wellwind@mail2000.com.tw>
2023-06-10 15:20:05 +0800

    任務 06：Reactive Forms 繫結表單資料

--- src/app/pages/posts/create/create.component.ts
+++ src/app/pages/posts/create/create.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormArray, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export default class CreateComponent {
+  formBuilder = inject(FormBuilder);
 
+  // body = this.formBuilder.control('');
+
+  // form = this.formBuilder.group({
+  //   title: this.formBuilder.control('Title 1'),
+  //   description: this.formBuilder.control(''),
+  //   body: this.body,
+  //   tags: this.formBuilder.array([])
+  // });
+
+  form = this.formBuilder.group({
+    title: this.formBuilder.control('Title 1'),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.formBuilder.array([
+
+      this.formBuilder.control('HTML'),
+      this.formBuilder.control('CSS'),
+      this.formBuilder.control('JavaScript'),
+
+    ])
+  });
+
+  get postBody() {
+    return this.form.get('body') as FormControl;
+  }
+
+  get postTags() {
+    return this.form.get('tags') as FormArray;
+  }
+
+  addTag(tag: string) {
+    // console.log(tag);
+    this.postTags.push(this.formBuilder.control(tag));
+  }
+
+  removeTag(index: number) {
+    this.postTags.removeAt(index);
+  }
+
+  createPost() {
+    console.log(this.form.value);
+  }
 }
